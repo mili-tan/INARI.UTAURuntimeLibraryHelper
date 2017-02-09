08538baeb3bea954779ffd6302595f5be551f637
Milkey Tan <819774989@qq.com>
2017-02-09 16:25:23 +0800

    修改UI

--- UTAURtLibHelper/loadForm.cs
+++ UTAURtLibHelper/loadForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using OneSet.EffectsWindow;
 
 namespace UTAURtLibHelper
 {
@@ -17,19 +18,19 @@ namespace UTAURtLibHelper
         {
             Thread.Sleep(500);
             InitializeComponent();
-            this.FormBorderStyle = FormBorderStyle.None;
+            FormBorderStyle = FormBorderStyle.None;
         }
 
         private void loadForm_Load(object sender, EventArgs e)
         {
-            TsWin.EffectsWindows(this.Handle, 1300, TsWin.AW_BLEND);
+            Fx.EffectsWindow(Handle, 1300, Fx.AW_BLEND);
         }
 
         private void timerShowMainForm_Tick(object sender, EventArgs e)
         {
-            this.Opacity = 0;
-            System.Windows.Forms.Form frmMain = new Form1();
-            this.Hide();
+            Opacity = 0;
+            Form frmMain = new Form1();
+            Hide();
             Thread.Sleep(100);
             frmMain.Show();
             timerShowMainForm.Enabled = false;
