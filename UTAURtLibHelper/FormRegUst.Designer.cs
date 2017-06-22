fbf227697697dcb5e414b0f195bc1f676bc81a2a
Milkey Tan <819774989@qq.com>
2017-06-22 15:23:47 +0800

    註冊文件關聯和反註冊

--- UTAURtLibHelper/FormRegUst.Designer.cs
+++ UTAURtLibHelper/FormRegUst.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.buttonOpenUTAUFile = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonRegFileLink = new System.Windows.Forms.Button();
+            this.buttonUnRegFileLink = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonOpenUTAUFile
@@ -39,7 +41,7 @@
             this.buttonOpenUTAUFile.Name = "buttonOpenUTAUFile";
             this.buttonOpenUTAUFile.Size = new System.Drawing.Size(357, 37);
             this.buttonOpenUTAUFile.TabIndex = 0;
-            this.buttonOpenUTAUFile.Text = "1.打开UTAU主程序";
+            this.buttonOpenUTAUFile.Text = "打开UTAU主程序";
             this.buttonOpenUTAUFile.UseVisualStyleBackColor = true;
             this.buttonOpenUTAUFile.Click += new System.EventHandler(this.buttonOpenUTAUFile_Click);
             // 
@@ -48,11 +50,36 @@
             this.openFileDialog.FileName = "utau.exe";
             this.openFileDialog.Filter = "UTAU主程序|utau.exe";
             // 
+            // buttonRegFileLink
+            // 
+            this.buttonRegFileLink.Enabled = false;
+            this.buttonRegFileLink.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.buttonRegFileLink.Location = new System.Drawing.Point(13, 56);
+            this.buttonRegFileLink.Name = "buttonRegFileLink";
+            this.buttonRegFileLink.Size = new System.Drawing.Size(357, 37);
+            this.buttonRegFileLink.TabIndex = 1;
+            this.buttonRegFileLink.Text = "注册文件关联";
+            this.buttonRegFileLink.UseVisualStyleBackColor = true;
+            this.buttonRegFileLink.Click += new System.EventHandler(this.buttonRegFileLink_Click);
+            // 
+            // buttonUnRegFileLink
+            // 
+            this.buttonUnRegFileLink.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.buttonUnRegFileLink.Location = new System.Drawing.Point(13, 99);
+            this.buttonUnRegFileLink.Name = "buttonUnRegFileLink";
+            this.buttonUnRegFileLink.Size = new System.Drawing.Size(357, 37);
+            this.buttonUnRegFileLink.TabIndex = 2;
+            this.buttonUnRegFileLink.Text = "取消注册文件关联";
+            this.buttonUnRegFileLink.UseVisualStyleBackColor = true;
+            this.buttonUnRegFileLink.Click += new System.EventHandler(this.buttonUnRegFileLink_Click);
+            // 
             // FormRegUst
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 203);
+            this.Controls.Add(this.buttonUnRegFileLink);
+            this.Controls.Add(this.buttonRegFileLink);
             this.Controls.Add(this.buttonOpenUTAUFile);
             this.Name = "FormRegUst";
             this.Text = "注册文件关联";
@@ -64,5 +91,7 @@
 
         private System.Windows.Forms.Button buttonOpenUTAUFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button buttonRegFileLink;
+        private System.Windows.Forms.Button buttonUnRegFileLink;
     }
 }
--- UTAURtLibHelper/FormRegUst.cs
+++ UTAURtLibHelper/FormRegUst.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,7 +26,61 @@ namespace UTAURtLibHelper
             if (Result == DialogResult.OK)
             {
                 utauFile = openFileDialog.FileName;
+                buttonRegFileLink.Enabled = true;
             }
+        }
+
+        public static void RegFile(string filePathStr, string fileTypeName)
+        {
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey("", true);
+            RegistryKey cmdKey = regKey.OpenSubKey(fileTypeName, true);
+            if (cmdKey != null)
+            {
+                regKey.DeleteSubKey(fileTypeName, true);
+            }
+            regKey.CreateSubKey(fileTypeName);
+            cmdKey = regKey.OpenSubKey(fileTypeName, true);
+            cmdKey.SetValue("", "Exec");
+            cmdKey = regKey.OpenSubKey("Exec", true);
+            if (cmdKey != null) regKey.DeleteSubKeyTree("Exec");
+            regKey.CreateSubKey("Exec");
+            cmdKey = regKey.OpenSubKey("Exec", true);
+            cmdKey.CreateSubKey("shell");
+            cmdKey = cmdKey.OpenSubKey("shell", true);
+            cmdKey.CreateSubKey("open");
+            cmdKey = cmdKey.OpenSubKey("open", true);
+            cmdKey.CreateSubKey("command");
+            cmdKey = cmdKey.OpenSubKey("command", true);
+            string pathStr = "\"" + filePathStr + "\" \"%1\"";
+            cmdKey.SetValue("", pathStr);
+        }
+
+        public static void UnRegFile(string fileTypeName)
+        {
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey("", true);
+            RegistryKey cmdKey = regKey.OpenSubKey(fileTypeName);
+            if (cmdKey != null)
+            {
+                regKey.DeleteSubKey(fileTypeName, true);
+            }
+            if (cmdKey != null)
+            {
+                regKey.DeleteSubKeyTree("Exec");
+            }
+        }
+
+        private void buttonRegFileLink_Click(object sender, EventArgs e)
+        {
+            RegFile(utauFile, ".ust");
+            RegFile(utauFile, ".uar");
+            MessageBox.Show("注册成功");
+        }
+
+        private void buttonUnRegFileLink_Click(object sender, EventArgs e)
+        {
+            UnRegFile(".ust");
+            UnRegFile(".uar");
+            MessageBox.Show("反注册成功");
         }
     }
 }
