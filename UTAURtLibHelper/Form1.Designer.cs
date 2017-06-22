622a1db54e8071e7d94c4b3583a240145371f988
Milkey Tan <819774989@qq.com>
2017-06-22 15:48:38 +0800

    Ver

--- UTAURtLibHelper/Form1.Designer.cs
+++ UTAURtLibHelper/Form1.Designer.cs
@@ -275,7 +275,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "UTAU Runtime Library Helper 2.3.2";
+            this.Text = "UTAU Runtime Library Helper 2.5";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
