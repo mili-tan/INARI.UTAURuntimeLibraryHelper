9b967e587db20ceccb986fcf732cf7f9c6a53065
Milkey Tan <819774989@qq.com>
2017-06-28 20:34:34 +0800

    注册

--- UTAURtLibHelper/FormLauncher.cs
+++ UTAURtLibHelper/FormLauncher.cs
@@ -11,14 +11,39 @@ namespace UTAURtLibHelper
 {
     public partial class FormLauncher : Form
     {
+        bool TestDlg32Reg;
+        bool TestComCTLReg;
+        string utauFile;
+
         public FormLauncher(string pathString)
         {
             InitializeComponent();
+
+            TestDlg32Reg = RegRun.RegTest(@"TypeLib\{F9043C88-F6F2-101A-A3C9-08002B2F49FB}\1.2\0\win32");
+            TestComCTLReg = RegRun.RegTest(@"TypeLib\{831FDD16-0C5C-11D2-A9FC-0000F8754DA1}\2.0");
+            utauFile = pathString;
         }
 
         private void FormLauncher_Load(object sender, EventArgs e)
         {
+            if (TestDlg32Reg == false)
+            {
+                RegRun.RegLib("/s " + Application.StartupPath + "/lib/COMDLG32.OCX");
+            }
+            if (TestComCTLReg == false)
+            {
+                RegRun.RegLib("/s " + Application.StartupPath + "/lib/MSCOMCTL.OCX");
+            }
 
+            try
+            {
+                RegUst.RegFile(utauFile, ".ust");
+                RegUst.RegFile(utauFile, ".uar");
+            }
+            catch (Exception errorMsg)
+            {
+                MessageBox.Show(errorMsg.Message);
+            }
         }
     }
 }
